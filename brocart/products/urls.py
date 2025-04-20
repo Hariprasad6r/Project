b0ade226576cf99f753bb9b1fa303baef05170e4
Hariprasad <hariprasadappu777@gmail.com>
2025-04-20 16:32:14 +0530

    Initial commit

---
+++ brocart/products/urls.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls.static import static
+from . import views
+
+urlpatterns = [
+    path('', views.index,name='home'),
+    path('products_list',views.list_products,name='list_product'),
+    path('product_detail/<pk>',views.detail_product,name='detail_product')
+]
---
+++ script.py
@@ -0,0 +1,8 @@
+a = 10
+b = 10
+c = a+b;
+print("The Result is",c)
+a = int(input("Enter a number:"))
+b = int(input("Enter a number"))
+c = a-b
+print(c)
